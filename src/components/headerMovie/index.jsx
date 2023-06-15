c57d65bc1371844f87bd146aaf2926636b778410
David Cotter <20062924@mail.wit.ie>
2023-06-15 10:56:16 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -5,30 +5,49 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const styles = {
-    root: {  
+  root: {
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    width: 60,
+    height: 60,
+  },
+  heartIcon: {
+    color: "red",
+    fontSize: 40,
+  },
 };
 
 const MovieHeader = (props) => {
   const movie = props.movie;
-
+  const favouriteMovies = JSON.parse(localStorage.getItem("favourites"));
+  const isFavorite = favouriteMovies.some((m) => m.id === movie.id);
+  
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
+      
+        {isFavorite ? (
+          <Avatar sx={styles.avatar}>
+          <FavoriteIcon style={styles.heartIcon} />
+          </Avatar>
+        ) : null}
+      
       <Typography variant="h4" component="h3">
-        {movie.title}{"   "}
+        {movie.title}
+        {"   "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="='large"/>
+          <HomeIcon color="primary" fontSize="large" />
         </a>
         <br />
         <span>{`${movie.tagline}`} </span>
